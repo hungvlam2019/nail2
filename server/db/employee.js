fecaf353db388ee98cc298fe8b6ef1a8e4c7f80a
Hung Lam <hvanlam2019@gmail.com>
2020-06-28 15:56:55 -0400

    fix update checkRate and payRate

--- server/db/employee.js
+++ server/db/employee.js
@@ -1,22 +1,22 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host     : 'localhost',
-    port     : 3306,
-    user     : 'root',
-    password : 'admin',
-    database : 'nail'
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'admin',
+    database: 'nail'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err)
         throw err;
     console.log("Connect to the database successfully");
 });
 
 module.exports = {
-    //TODO - need to provide validation
-    insert : function(firstName, lastName, nickName, phoneNumber, checkRate=0.6, payRate=0.6, callback) {
+
+    insert: function (firstName, lastName, nickName, phoneNumber, checkRate = 0.6, payRate = 0.6, callback) {
         var sql = "INSERT INTO employee (firstName, lastName, nickName, phoneNumber, checkRate, payRate) VALUES (?, ?, ?, ?, ?, ?)";
         connection.query(sql, [firstName, lastName, nickName, phoneNumber, checkRate, payRate], function (err, result) {
             if (err)
@@ -25,9 +25,21 @@ module.exports = {
                 callback(result);
         });
     },
-    get : function(employeeId, callback) {
-        var sql = 'SELECT id, firstName, lastName, nickName, phoneNumber, checkRate, payRate FROM employee WHERE id = "' + employeeId + '" LIMIT 1';
-        connection.query(sql, employeeId, function(err, result) {
+
+    update: function (employeeId, firstName, lastName, nickName, phoneNumber, checkRate, payRate, active, callback) {
+        var sql = 'UPDATE employee SET firstName = ?, lastName = ?, nickName = ?, phoneNumber = ?, checkRate = ?, payRate = ?, active = ? WHERE id = ?';
+        connection.query(sql, [firstName, lastName, nickName, phoneNumber, checkRate, payRate, active, employeeId], function (err, result) {
+            if (err)
+                throw err;
+            if (callback !== undefined) {
+                callback(result);
+            }
+        });
+    },
+
+    get: function (employeeId, callback) {
+        var sql = 'SELECT id, firstName, lastName, nickName, phoneNumber, checkRate, payRate, active FROM employee WHERE id = "' + employeeId + '" LIMIT 1';
+        connection.query(sql, employeeId, function (err, result) {
             if (err) {
                 throw err;
             }
@@ -36,33 +48,25 @@ module.exports = {
             }
         });
     },
-    getAll : function(callback) {
-        var sql = 'SELECT id, firstName, lastName, nickName, phoneNumber, checkRate, payRate FROM employee';
-        connection.query(sql, function(err, result) {
+
+    getAll: function (callback) {
+        var sql = 'SELECT id, firstName, lastName, nickName, phoneNumber, checkRate, payRate, active FROM employee';
+        connection.query(sql, function (err, result) {
             if (!err) {
                 callback(result);
-            } else {
+            }
+            else {
                 //TODO - need to pass error response to client
                 console.log(err);
                 throw err;
             }
         });
     },
-    update : function(employeeId, firstName, lastName, nickName, phoneNumber, checkRate, payRate, callback) {
-        var sql = 'UPDATE employee SET firstName = "' + firstName + '", lastName = "' + lastName + '", nickName = "' + nickName + '", phoneNumber = "' + phoneNumber
-                  + '", checkRate = "' + checkRate + '", payRate = "' + payRate + '" WHERE id = "' + employeeId + '"';
-        connection.query(sql, function(err, result) {
-            if (err)
-                throw err;
-            if (callback !== undefined) {
-                callback(result);
-            }
-        });
-    },
-    delete : function(employeeId, callback) {
+
+    delete: function (employeeId, callback) {
         var sql = 'DELETE FROM employee WHERE id = "' + employeeId + '"';
         console.log('Delete: ' + sql);
-        connection.query(sql, employeeId, function(err, result) {
+        connection.query(sql, employeeId, function (err, result) {
             if (err)
                 throw err;
             if (callback !== undefined) {
@@ -70,4 +74,4 @@ module.exports = {
             }
         });
     }
-};
+}
--- server/routes/employees.js
+++ server/routes/employees.js
@@ -13,7 +13,6 @@ module.exports = function(app) {
     
         //TODO : handle error case in res.send(result)
         employee.insert(firstName, lastName, nickName, phoneNumber, checkRate, payRate, function(result) {
-            //res.sendFile('pages/showEmployees.html', {root: __dirname});
             res.send(result);
         });
     });
@@ -21,17 +20,23 @@ module.exports = function(app) {
     app.put('/employees/:id', (req, res) => {
         var employeeId = parseInt(req.params.id);
         employee.get(employeeId, (result) => {
-            console.log('PUT: ' + employeeId + ' : ' + JSON.stringify(result));
-            console.log('PUT: ' + employeeId + ' <> ' + result.id);
+            console.log('PUT: ' + employeeId + ' : ' + req.body.payRate);
             if (result && result.id === employeeId) {
                 console.log('Ready to update employee: ' + employeeId);
-                var firstName = req.body.firstName;
-                var lastName = req.body.lastName;
-                var nickName = req.body.nickName || firstName;
-                var phoneNumber = req.body.phoneNumber;
+                var firstName = req.body.firstName || result.firstName;
+                var lastName = req.body.lastName || result.lastName;
+                var nickName = req.body.nickName || result.nickName;
+                var phoneNumber = req.body.phoneNumber || result.phoneNumber;
                 var checkRate = req.body.checkRate;
+                if (checkRate === undefined || payRate === null) {
+                    checkRate = result.checkRate;
+                }
                 var payRate = req.body.payRate;
-                employee.update(employeeId, firstName, lastName, nickName, phoneNumber, checkRate, payRate, (result) => {
+                if (payRate === undefined || payRate === null) {
+                    payRate = result.payRate;
+                }
+                var active = req.body.active || result.active;
+                employee.update(employeeId, firstName, lastName, nickName, phoneNumber, checkRate, payRate, active, (result) => {
                     res.send(result);
                 });
             }
